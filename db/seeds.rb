dea4cb840871f101b248ff51f6ac60ef8e6f02c4
Henry Thomas <henrykun95@gmail.com>
2025-05-30 09:03:09 +0100

    testing seedfile countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+require 'open-uri'
+
+puts 'cleaning database...'
+Country.destroy_all
+
+url = 'https://restcountries.com/v3.1/name/deutschland'
+
+countries_json = URI.parse(url).read
+countries = JSON.parse(countries_json)
+
+countries.each do |country|
+  puts 'creating country...'
+  currency_hash = country['currencies'].values[0]
+  Country.create!(
+    name: country['name'].values[0],
+    currency: currency_hash.values[1],
+    capital: country['capital'][0],
+    language_one: country['languages'].values[0],
+    continent: country['region'],
+    region: country['subregion'],
+    flag: country['flags'].values[0],
+    timezone: country['timezones'][0],
+    population: country['population'],
+    latitude: country['latlng'][0],
+    longitude: country['latlng'][1]
+  )
+end
+
+puts "finished! created #{Country.count} countries!"
