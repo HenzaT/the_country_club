aadc97749820ec69e8d2477c3aab2db316ea9b5f
Henry Thomas <henrykun95@gmail.com>
2025-05-31 12:22:10 +0100

    added show page for country

--- app/controllers/countrys_controller.rb
+++ app/controllers/countrys_controller.rb
@@ -1,5 +1,16 @@
 class CountrysController < ApplicationController
-  def index
-    @europe = Country.where(continent: 'Europe')
+  # home, continents, country
+  def home
+    @all = Country.all
+  end
+
+  # show individual continent
+  def show
+    @continents = Country.select('continent').group('continent')
+  end
+
+  # show individual country
+  def show
+    @country = Country.find(params[:id])
   end
 end
