0d8a866e5df475d4aee9063d712e8d84dcf51d3a
Henry Thomas <henrykun95@gmail.com>
2025-06-01 13:51:03 +0100

    changed americas to regions, created regions controller

--- app/controllers/countrys_controller.rb
+++ app/controllers/countrys_controller.rb
@@ -2,17 +2,13 @@ class CountrysController < ApplicationController
   # home, continents, show
   def home
     @continents = Country.select('continent').group('continent')
-    @africa = Country.where(continent: 'Africa')
-    @europe = Country.where(continent: 'Europe')
-    @asia = Country.where(continent: 'Asia')
-    @oceania = Country.where(continent: 'Oceania')
-    @north_america = Country.where(region: 'North America')
-    @central_america = Country.where(region: 'Central America')
-    @south_america = Country.where(region: 'South America')
-  end
-
-  # show individual continent
-  def continent
+    # @africa = Country.where(continent: 'Africa')
+    # @europe = Country.where(continent: 'Europe')
+    # @asia = Country.where(continent: 'Asia')
+    # @oceania = Country.where(continent: 'Oceania')
+    # @north_america = Country.where(region: 'North America')
+    # @central_america = Country.where(region: 'Central America')
+    # @south_america = Country.where(region: 'South America')
   end
 
   # show individual country
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-  resources :countrys, only: %i[home show] do
-    member do
-      get 'continent'
-    end
-  end
+  resources :countrys, only: %i[home show]
+  resources :continents, only: [:show]
+  resources :regions, only: [:show]
 
   # Defines the root path route ("/")
   root 'countrys#home'
