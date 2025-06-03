37dc48fdfdace42ae06b8b95428b72b7e8d1a3e4
Henry Thomas <henrykun95@gmail.com>
2025-06-03 22:35:13 +0100

    edit wishlist added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
   resources :countries, only: %i[home show] do
-    resources :wishlists, only: %i[new create]
+    resources :wishlists, only: %i[new create edit update destroy]
   end
   resources :continents, only: [:show]
   resources :regions, only: [:show]
