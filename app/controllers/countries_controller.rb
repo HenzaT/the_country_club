b6b6c5be555af02c93e2d045d0b883c662b28d2e
Henry Thomas <henrykun95@gmail.com>
2025-06-07 15:10:30 +0100

    built dashboard page, added favourites and wishlist to cards

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,23 +2,19 @@ class CountriesController < ApplicationController
   # home, continents, show
   def home
     @continents = Country.select('continent').group('continent')
-    # @africa = Country.where(continent: 'Africa')
-    # @europe = Country.where(continent: 'Europe')
-    # @asia = Country.where(continent: 'Asia')
-    # @oceania = Country.where(continent: 'Oceania')
-    # @north_america = Country.where(region: 'North America')
-    # @central_america = Country.where(region: 'Central America')
-    # @south_america = Country.where(region: 'South America')
   end
 
   # show individual country
   def show
     @country = Country.find(params[:id])
     @user_wishlists = @country.wishlists.where(user_id: current_user)
-    @wishlist_already_created = Wishlist.exists?(user_id: current_user, country_id: @country)
-    @wishlist = Wishlist.where(user_id: current_user, country_id: @country)
     @user_favourites = @country.favourites.where(user_id: current_user)
+    @wishlist_already_created = Wishlist.exists?(user_id: current_user, country_id: @country)
     @favourite_already_created = Favourite.exists?(user_id: current_user, country_id: @country)
-    @favourite = Favourite.where(user_id: current_user, country_id: @country)
+  end
+
+  def dashboard
+    # left_outer_joins gets all records that have favourite, wishlists (like joins), then specify with where to filter
+    @countries = Country.left_outer_joins(:favourites, :wishlists).where('favourites.user_id = ? OR wishlists.user_id = ?', current_user.id, current_user.id).distinct
   end
 end
