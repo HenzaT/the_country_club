e4e4e9511d0bc80d71b3cc7117be26ab76c04747
Henry Thomas <henrykun95@gmail.com>
2025-06-19 22:38:27 +0100

    styling country cards

--- frontend/controllers/navbar_controller.ts
+++ frontend/controllers/navbar_controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("Hello from navbar controller")
+  static targets: string[] = [ 'hamburger', 'body', 'downArrow', 'dropdownMenu' ]
+
+  toggleBody(): void {
+    (this.targets.find('body') as HTMLElement)?.classList.toggle('active');
+  }
+
+  toggleDropdownMenu(): void {
+    (this.targets.find('dropdownMenu') as HTMLElement)?.classList.toggle('open');
   }
 }
