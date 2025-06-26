c2e72cca051b3c7981c42b4690371a6e8e4b99ea
Henry Thomas <henrykun95@gmail.com>
2025-06-26 22:42:52 +0100

    fixing concern for random photo

--- app/controllers/continents_controller.rb
+++ app/controllers/continents_controller.rb
@@ -1,8 +1,11 @@
 class ContinentsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
+  include RandomPhoto
 
   def show
     @continent = params[:id]
     @countries = Country.where(continent: @continent)
+
+    get_random_unsplash_photo
   end
 end
