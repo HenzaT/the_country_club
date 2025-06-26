92a57178e5efdedc93c8247e84d663e3d35dca25
Henry Thomas <henrykun95@gmail.com>
2025-06-26 22:01:32 +0100

    added display image to continents pages

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,6 +1,7 @@
 class RegionsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
-  
+  include Random_Photo
+
   def show
     @continent_region = params[:id]
     @countries = Country.where(region: @continent_region)
