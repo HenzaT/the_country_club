2851b27378b6400ff984219be8ed9d2fe09ba127
Henry Thomas <henrykun95@gmail.com>
2025-06-26 22:01:32 +0100

    added display image to continents pages

--- frontend/entrypoints/application.ts
+++ frontend/entrypoints/application.ts
@@ -1,17 +1,12 @@
 import { Application } from "@hotwired/stimulus"
-import { Chart, registerables } from 'chart.js';
 import ChartController from "../controllers/chart_controller"
 import NavbarController from "../controllers/navbar_controller"
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 const application = Application.start()
 application.register("chart", ChartController)
-Chart.register(...registerables);
 application.register("navbar", NavbarController)
-// To see this message, follow the instructions for your Ruby framework.
-//
-// When using a plain API, perhaps it's better to generate an HTML entrypoint
-// and link to the scripts and stylesheets, and let Vite transform it.
 
-// Example: Import a stylesheet in <sourceCodeDir>/index.css
-// import '~/index.css'
 import '../stylesheets/application.scss'
