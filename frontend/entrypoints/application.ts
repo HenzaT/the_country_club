8f0c394cf40463890c1f1a3829c611a49dbc404b
Henry Thomas <henrykun95@gmail.com>
2025-06-29 16:07:21 +0100

    added mapbox to show country coordinates, refactored region page

--- frontend/entrypoints/application.ts
+++ frontend/entrypoints/application.ts
@@ -3,6 +3,7 @@ import ChartController from "../controllers/chart_controller"
 import ClockController from "../controllers/clock_controller"
 import DatepickerController from "../controllers/datepicker_controller"
 import NavbarController from "../controllers/navbar_controller"
+import MapController from "../controllers/map_controller"
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
@@ -12,5 +13,10 @@ application.register("chart", ChartController)
 application.register("clock", ClockController)
 application.register("datepicker", DatepickerController)
 application.register("navbar", NavbarController)
+application.register("map", MapController)
 
 import '../stylesheets/application.scss'
+
+//
+//*= require mapbox-gl
+//= require_tree .
