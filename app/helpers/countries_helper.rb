062fdd213a9ae2555de0a54603ce8127125a05a5
Henry Thomas <henrykun95@gmail.com>
2025-07-06 20:47:36 +0100

    experimenting with helper methods

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -1,5 +1,26 @@
 module CountriesHelper
-  def population_people
-
+  def population_people(country)
+    case
+    when country.population <= 100
+      '100'
+    when country.population <= 500
+      '500'
+    when country.population <= 1000
+      '1000'
+    when country.population <= 5000
+      '5000'
+    when country.population <= 10000
+      '10000'
+    when country.population <= 100000
+      '100000'
+    when country.population <= 1000000
+      '1000000'
+    when country.population <= 10000000
+      '10000000'
+    when country.population <= 100000000
+      '100000000'
+    else
+      "that's more"
+    end
   end
 end
--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -1,2 +1,9 @@
 module RegionsHelper
+  def sort_alphabetically(countries)
+    names = []
+    countries.each do |country|
+      names << country.name
+    end
+    names.sort
+  end
 end
