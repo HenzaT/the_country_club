f00dae20f3d310124f56b5d20ad751a765ec3ec6
Henry Thomas <henrykun95@gmail.com>
2026-02-23 07:46:08 +0000

    styled wishlist favourite cards, added currency switch button

--- frontend/controllers/currency_controller.ts
+++ frontend/controllers/currency_controller.ts
@@ -1,10 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets: string[] = [ 'switchBtn' as const ];
+  declare readonly switchBtnTarget: HTMLButtonElement;
+
   connect() {
     console.log("Hello from Currency controller")
   }
 
+  switchInputs() {
+    console.log('Hello!')
+  }
   // get value of input for GBP. Example, if 2 GBP, then multiply the right hand side accordingly.
   // example 2 GBP = currency * 2
 }
