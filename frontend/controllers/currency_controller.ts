15a6e4a1c632ed5b27d2bc52c95990071cb65363
Henry Thomas <henrykun95@gmail.com>
2026-03-02 21:13:53 +0000

    fixed navbar styling, added currency change buttons

--- frontend/controllers/currency_controller.ts
+++ frontend/controllers/currency_controller.ts
@@ -10,7 +10,27 @@ export default class extends Controller {
   }
 
   exchange() {
-    
+  }
+
+  buttonChange(event: any) {
+    const value = event.params.value;
+    const gbpAsNum = Number(this.gbpInputTarget.value);
+    const foreignNum = Number(this.foreignCurrencyInputTarget.value);
+
+    switch (value) {
+      case 200:
+        this.gbpInputTarget.value = (gbpAsNum * 2).toString();
+        this.foreignCurrencyInputTarget.value = (foreignNum * 2).toString();
+        break
+      case 400:
+        this.gbpInputTarget.value = (gbpAsNum * 4).toString();
+        this.foreignCurrencyInputTarget.value = (foreignNum * 4).toString();
+        break
+      case 600:
+        this.gbpInputTarget.value = (gbpAsNum * 6).toString();
+        this.foreignCurrencyInputTarget.value = (foreignNum * 6).toString();
+        break
+    }
   }
   // get value of input for GBP. Example, if 2 GBP, then multiply the right hand side accordingly.
   // example 2 GBP = currency * 2
